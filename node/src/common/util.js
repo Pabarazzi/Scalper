f4f2e35813c2b087e5c2dda45637b1ed6dba57cf
宋立岭 <songliling@oneapm.com>
2018-08-13 22:26:28 +0800

    1、 fix domain to ip list 2、 find fast ip

---
+++ node/src/common/util.js
@@ -0,0 +1,47 @@
+var dns = require('dns')
+var tcpp = require('tcp-ping')
+
+const domainToIpV4Promise = async function (domain) {
+  return new Promise((resolve, reject) => {
+    if (!domain) {
+      reject('Error, domain is null')
+    }
+    dns.resolve4(domain, (err, result) => {
+      resolve(result)
+    })
+  })
+}
+
+const domainToIpV4 = async function (domain) {
+  const result = await domainToIpV4Promise(domain)
+  return result
+}
+
+const test = function (ip) {
+  return new Promise((resolve, reject) => {
+    tcpp.ping({ address: ip }, function(err, data) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+function compare (t1, t2) {
+  return t1.avg - t2.avg
+}
+
+async function fastDomain(domain) {
+  if (!domain) return
+  const result = await domainToIpV4(domain)
+  if (!result || result.length === 0) return
+  const promises = result.map((item) => test(item))
+  const results = await Promise.all(promises)
+  return results.sort(compare)[0]
+}
+
+
+
+export { fastDomain }
---
+++ node/src/index.js
@@ -0,0 +1,5 @@
+import { fastDomain } from './common/util'
+
+fastDomain('www.228.com.cn').then(result => console.log(result.address))
+
+
