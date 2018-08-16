e739fcd164357d988ac589060d96f2013cac8a0e
宋立岭 <songliling@oneapm.com>
2018-08-16 22:34:36 +0800

    1、fix fast domain dns list TTL

--- node/src/common/util.js
+++ node/src/common/util.js
@@ -1,5 +1,6 @@
 var dns = require('dns')
 var tcpp = require('tcp-ping')
+var nslookup = require('nslookup')
 
 const domainToIpV4Promise = async function (domain) {
   return new Promise((resolve, reject) => {
@@ -35,8 +36,41 @@ const TTL = function (ip) {
   })
 }
 
+function lookupPromise(ip, domain) {
+	return new Promise((resolve, reject) => {
+		nslookup(domain)
+			.server(ip) // default is 8.8.8.8
+			.type('a') // default is 'a'
+			.timeout(3 * 1000) // default is 3 * 1000 ms
+			.end(function (err, addrs) {
+				resolve(addrs)
+			});
+	})
+}
+
+/**
+ * 排序
+ * @param t1
+ * @param t2
+ * @returns {number}
+ */
 function compare (t1, t2) {
   return t1.avg - t2.avg
+}
+
+/**
+ * 展开二维数组
+ * @param arr
+ * @returns {*}
+ */
+function formatArr(arr) {
+	return arr.reduce((o, v) => {
+		return o.concat(v)
+	}, [])
+}
+
+function removeRepeat(arr) {
+	return [... new Set(arr)]
 }
 
 /**
@@ -46,9 +80,9 @@ function compare (t1, t2) {
  * @returns {Promise.<T|*>}
  */
 async function fastDomain(domain, dnsList) {
-  if (!domain) return
-  const promises = dnsList.map((item) => TTL(item))
-  const results = await Promise.all(promises)
+	let targetIpList = await Promise.all(dnsList.map((item) => lookupPromise(item, domain)))
+	targetIpList = removeRepeat(formatArr(targetIpList)).filter(item => item !== void(0))
+  const results = await Promise.all(targetIpList.map((item) => TTL(item)))
   return results.sort(compare)[0]
 }
 
--- node/src/index.js
+++ node/src/index.js
@@ -1,13 +1,21 @@
 const fastDomain = require('./common/util');
-const nslookup = require('nslookup');
 const domain = 'www.228.com.cn';
 const DNS = require('./router/dns');
 
-
 const dnsPromise = new DNS();
 
-dnsPromise.then(instance => {
-    console.log(instance.listServerIp());
-}).catch(e => {
-    console.log(e)
-})
+async function getFastDomain() {
+	const instance = await dnsPromise
+	const dnsList = instance.listServerIp().map(item => item.vip)
+	fastDomain(domain, dnsList).then(result => console.log(result))
+}
+
+getFastDomain()
+
+// dnsPromise.then(instance => {
+// 	const dnsList = instance.listServerIp().map(item => item.vip)
+// 	fastDomain(domain, dnsList).then(result => console.log(result))
+// }).catch(e => {
+// 	console.log(e)
+// })
+
